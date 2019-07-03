e73c0e629aba0457ec0bfdf2d22d56a10b769212
ravi-ashara <raviashar94@gmail.com>
2019-07-03 23:48:17 +0530

    Change navigate path

--- src/app/services/authGaurd/auth-gaurd.service.ts
+++ src/app/services/authGaurd/auth-gaurd.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRouteSnapshot, CanActivate } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Injectable({
@@ -7,14 +7,13 @@ import { NavController } from '@ionic/angular';
 })
 export class AuthGaurdService implements CanActivate {
 
-  constructor(public router: Router, public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController) { }
 
-  canActivate(route: ActivatedRouteSnapshot): boolean {
+  canActivate() {
     if (localStorage.getItem('isLogin') === 'true') {
-      // this.navCtrl.navigateRoot('/home');
       return true;
     } else {
-      this.router.navigateByUrl('/login');
+      this.navCtrl.navigateRoot(['/login']);
       return false;
     }
   }
